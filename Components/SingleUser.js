2c32e0c19385aa430e42a82b2787f8d45bc07dab
voidxyzvoidxyz <42205781+voidxyzvoidxyz@users.noreply.github.com>
2020-08-01 15:28:01 +0530

    Update SingleUser.js

--- Components/SingleUser.js
+++ Components/SingleUser.js
@@ -1,1 +1,23 @@
 
+import React from 'react'
+
+class SingleUser extends React.Component {
+    
+   componentDidMount() {   
+   this.props.fsingle(this.props.match.params.login)
+   console.log(this.props)
+   }
+    
+    render(){
+        return(
+          <div>
+              {this.props.user.login}
+              {this.props.user.public_repos}
+            
+              <h1>Welcome</h1>
+          </div>
+
+        )
+    }
+}
+export default SingleUser;
