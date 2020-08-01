76f706d76be841076eac13ba9ad3b8be371b21cf
voidxyzvoidxyz <42205781+voidxyzvoidxyz@users.noreply.github.com>
2020-08-01 15:26:19 +0530

    Update About.js

--- Components/About.js
+++ Components/About.js
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import { render } from '@testing-library/react';
+
+class About extends React.Component {
+
+
+render() {
+    return(
+
+        <div>
+            <h1>Welcome to about page</h1>
+        </div>
+    )
+
+    }
+
+}
+export default About;
