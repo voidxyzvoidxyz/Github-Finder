26ccb0f3603416b4b8efcb5647b446735d78b528
voidxyzvoidxyz <42205781+voidxyzvoidxyz@users.noreply.github.com>
2020-08-01 15:26:52 +0530

    Update Alert.js

--- Components/Alert.js
+++ Components/Alert.js
@@ -1,1 +1,19 @@
 
+import React from 'react';
+
+class Alert extends React.Component{
+
+
+    render(){
+        return(
+            <div>
+                {this.props.msg !== null && (
+                    <div className ={this.props.msg.type}>
+                        <h3>{this.props.msg.msg}</h3>
+                        </div>
+                )}
+            </div>
+        )
+    }
+}
+export default Alert;
