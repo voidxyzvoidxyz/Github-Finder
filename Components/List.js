b82ffeed7e6bbf44bb51dfe8b3049ac2b97ba451
voidxyzvoidxyz <42205781+voidxyzvoidxyz@users.noreply.github.com>
2020-08-01 15:27:26 +0530

    Update List.js

--- Components/List.js
+++ Components/List.js
@@ -1,1 +1,26 @@
 
+import React from 'react';
+import {Link} from 'react-router-dom';
+import SingleUser from './SingleUser';
+import { render } from '@testing-library/react';
+
+class List extends React.Component {
+  
+      
+
+
+render(){
+    return(
+      <main>
+      <div className = "container">
+          <img src = {this.props.cool.avatar_url}></img>
+          <h3> { this.props.cool.login}</h3>
+          <h3>{this.props.cool.node_id}</h3>
+            
+          <Link to = {`/SingleUser/${this.props.cool.login}`}>More</Link>    
+      </div>
+      </main>  
+    )
+}
+}
+export default List;
